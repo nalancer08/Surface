e6e297bab2c15e7a67138ba34905b10307522d61
Erick Sanchez <nuk.les@hotmail.com>
2017-09-26 15:19:30 -0500

    Added base project

--- Java/Core/SfObject.java
+++
@@ -1,9 +0,0 @@
-package com.appbuilders.quienesestepoke.libraries.Surface.Core;
-
-public class SfObject extends Object {
-
-    /*@Override
-    public String toString() {
-        return new JSONObject();
-    }*/
-}
