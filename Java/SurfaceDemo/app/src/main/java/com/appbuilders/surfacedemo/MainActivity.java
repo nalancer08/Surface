d18c629a332959cf5c2f12e35fe16c30f52869a5
Erick Sanchez <nuk.les@hotmail.com>
2017-10-06 22:47:20 -0500

    Added support

--- Java/SurfaceDemo/app/src/main/java/com/appbuilders/surfacedemo/MainActivity.java
+++ Java/SurfaceDemo/app/src/main/java/com/appbuilders/surfacedemo/MainActivity.java
@@ -3,7 +3,9 @@ package com.appbuilders.surfacedemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.appbuilders.surface.SfControl;
 import com.appbuilders.surface.SfSize;
+import com.appbuilders.surfacedemo.Controls.TabBar;
 import com.appbuilders.surfacedemo.View.MainView;
 
 public class MainActivity extends AppCompatActivity {
