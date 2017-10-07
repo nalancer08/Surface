3d8fb874541a6a3967399c0afd386f3700b95a4e
Erick Sanchez <nuk.les@hotmail.com>
2017-10-06 22:49:05 -0500

    Added base for SfWidget

--- Java/SurfaceDemo/surface/src/main/java/com/appbuilders/surface/SfWidget.java
+++ Java/SurfaceDemo/surface/src/main/java/com/appbuilders/surface/SfWidget.java
@@ -1,8 +1,20 @@
 package com.appbuilders.surface;
 
+import android.content.Context;
+
 /**
  * Created by ercsanchez on 26/09/17.
  */
 
-public class SfWidget {
+public class SfWidget extends SfScreen {
+
+    /**
+     * Constructor to define the context and get screen size
+     * @param context : Application context
+     */
+    public SfWidget(Context context) {
+        super(context);
+    }
+
+
 }
